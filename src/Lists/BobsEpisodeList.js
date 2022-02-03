b09788b94cadb8a340b523c27226d8f697e6a0df
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-03 15:12:25 -0800

    css on Bobs List

--- src/Lists/BobsEpisodeList.js
+++ src/Lists/BobsEpisodeList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 export default function BobsEpisodeList({ bobEpisode }) {
-  return <section>
+  return <section className='episode-list'>
     {
       bobEpisode.map(({
         title,
         holiday,
         episode,
         description
-      }, i) => <div key={title + i}>
+      }, i) => <div key={title + i} className='episode'>
         <h3>{title}</h3>
-        <p>{holiday}, {episode}</p>
-        <p>{description}</p>
+        <p className='info'>{holiday}, {episode}</p>
+        <p className='description'>{description}</p>
       </div>)
     }
   </section>;
