e1399a342030ac15d800abee797fae0e5f3c1c78
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-03 12:44:22 -0800

    set states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import BobsEpisodeList from './Lists/BobsEpisodeList';
+import FoodList from './Lists/FoodList';
+import HolidayList from './Lists/HolidayList';
+import PetList from './Lists/PetList';
+
 
 function App() {
+  const [bobEpisode, setBobEpisode] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [holidays, setHolidays] = useState([]);
+  const [myPets, setMyPets] = useState([]);
+
   return (
     <div className="App">
-      
+      <h2>List of Bob&apos;s Burgers Holiday Episodes</h2>
+      <h2>List of Amazing Foods</h2>
+      <h2>List of Holidays</h2>
+      <h2>Rankings of My Pets</h2>
     </div>
   );
 }
